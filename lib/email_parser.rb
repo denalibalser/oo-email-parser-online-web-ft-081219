65db4d3f3e1fa4f091b44f2291b7dc187e3e49d4
Denali Balser <denali.balser167@gmail.com>
2019-08-27 15:51:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,11 +6,12 @@
 class EmailAddressParser
   attr_accessor :email_addresses
 
-  def initialize
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
   end
 
   def parse
     @email_addresses.split(',' || ' ')
-    
+
   end
 end
